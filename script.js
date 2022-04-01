26b34d451e35a7ffbf7217cf82b24b6e9b083dfb
Caio Lima <cbernardodelima@ucf.edu>
2022-04-01 12:16:13 -0400

    Functions

---
+++ script.js
@@ -0,0 +1,30 @@
+alert("The name Caio starts with the letter C")
+function firstLetterName(name)
+         {
+         alert("The name " + name + " starts with the letter " + name[0]);
+               
+         }      
+function divisibleByTwo(value)
+        {
+             if(value % 2 === 0)
+             {
+             return true;
+             }
+             else
+                {
+             return false;
+                }
+function largestNum(arr)
+         {
+          let max = arr[0];
+
+         for(i = 0; i < arr.length; i++)
+             {
+         if(max < arr[i])
+             {
+             max = arr[i];
+             }
+             }
+         return max; 
+         }}
+         
